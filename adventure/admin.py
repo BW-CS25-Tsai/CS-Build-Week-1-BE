632d86629bbd2ab1ed74880450fd287590942708
Tashi Woeser <tashiwoeser@gmail.com>
2020-02-04 14:37:37 -0800

    connected room and player class in adminpy

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Room, Player
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 2
+
+class RoomAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['title']}),
+    ('Description', {'fields': ['description'], 'classes': ['collapse']}),]
+    inline = [PlayerInline]
+
+# admin.site.register(Room)
+# admin.site.register(Player)
+admin.site.register(Room, RoomAdmin)
+
