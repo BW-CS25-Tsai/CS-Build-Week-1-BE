d3a01637c311d25594e26fc4dc325c3b9d86cf03
tsaihuangsd <tsaihuangsd@gmail.com>
2020-02-05 11:21:33 -0800

    add del DATABASES['default']['OPTIONS']['sslmode'] to end of settings.py to fix sslmode problem

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -102,6 +102,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -158,3 +160,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import django_heroku
 django_heroku.settings(locals())
+
+del DATABASES['default']['OPTIONS']['sslmode']
